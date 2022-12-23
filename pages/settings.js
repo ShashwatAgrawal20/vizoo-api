f54db1b186c2e4f288104978d8ea95a56baa2c5c
Shashwat <shashwat>
2022-12-23 22:19:37 +0530

    added settings & about page & did some hover styling

---
+++ pages/settings.js
@@ -0,0 +1,10 @@
+export default function settings() {
+    return (
+        <>
+        <center><h1>Settings Page</h1></center>
+        <div style={{display: "flex", alignItems: "center", justifyContent: "center", padding: "25px"}}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur ullam veniam animi quidem molestiae ad accusantium quas aspernatur alias hic minus nobis esse quae voluptatem assumenda nihil reiciendis delectus sapiente, fugiat quibusdam quia architecto! Excepturi voluptas facilis ipsum sapiente modi at vel possimus aut ducimus iure expedita sequi ratione, enim ad saepe. Obcaecati, iste dolorem beatae molestiae illo commodi sit pariatur. Optio quam ad similique cumque dolorem eligendi et deserunt eius in! Maiores similique molestias dolorem enim eum sapiente accusantium consequatur. Provident, incidunt necessitatibus ipsa aspernatur dolore quas dolorum quod doloremque quo veniam minima mollitia repellat illo earum rem perspiciatis.
+        </div>
+        </>
+    ) 
+}
